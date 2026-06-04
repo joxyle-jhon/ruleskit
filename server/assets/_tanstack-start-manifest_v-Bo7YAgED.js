8749a21a13200b580f961819918f11cbfb5c2a20
Jhon Lloyd F. Omblero <omblero.jhonlloyd.04@gmail.com>
2026-06-04 18:24:00 +0800

    Updates

---
+++ server/assets/_tanstack-start-manifest_v-Bo7YAgED.js
@@ -0,0 +1,4 @@
+const tsrStartManifest = () => ({ routes: { __root__: { filePath: "D:/NEW/Github/Fronend Engineer/frontend-rules/src/routes/__root.tsx", children: ["/"], preloads: ["/assets/index-C7gm57S4.js"], scripts: [{ attrs: { type: "module", async: true, src: "/assets/index-C7gm57S4.js" } }] }, "/": { filePath: "D:/NEW/Github/Fronend Engineer/frontend-rules/src/routes/index.tsx", children: void 0, preloads: ["/assets/index-B5O1Wy4m.js"] } } });
+export {
+  tsrStartManifest
+};
